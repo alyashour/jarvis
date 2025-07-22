95d4e405f2efce11211e25c48501d9668e18fd07
Aly Ashour <alyashour1@gmail.com>
2025-07-22 12:47:42 -0400

    clean

--- cli/src/commands/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod task_groups;
-pub mod task;
-pub mod task_priorities;
-pub mod debug;
-
-pub use task::TaskArgs;
-pub use task_groups::TaskGroupArgs;
-pub use task_priorities::TaskPrioritiesArgs;
-pub use debug::DebugArgs;
--- cli/src/priorities.rs
+++
@@ -1,9 +0,0 @@
-use clap::ValueEnum;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
-pub enum Priority {
-    Low,
-    #[clap(alias = "med")]
-    Medium,
-    High
-}
